a635e754f11a63b1e30a095af87576b22d36f18e
lakshay <lakshay@lakshays-MacBook-Pro.local>
2024-08-10 12:52:02 +0530

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,28 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+
+class RedisConcurrentTask implements Runnable{
+
+    @Override
+    public void run() {
+
+
+    }
+}
 
 public class Main {
+
+
+
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
+    ExecutorService executorService = Executors.newFixedThreadPool(10);
 
       //Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
@@ -16,18 +33,13 @@ public class Main {
           // Since the tester restarts your program quite often, setting SO_REUSEADDR
           // ensures that we don't run into 'Address already in use' errors
           serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-          String clientInput = null;
 
-
-          OutputStream outputStream = clientSocket.getOutputStream();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            while((clientInput = bufferedReader.readLine())!=null){
-                if(clientInput.equalsIgnoreCase("ping")) {
-                    outputStream.write("+PONG\r\n".getBytes());
-                }
-            }
+          while( true){
+              // Wait for connection from client.
+              clientSocket = serverSocket.accept();
+              Socket finalClientSocket = clientSocket;
+              executorService.submit(()-> processRequest(finalClientSocket));
+          }
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
@@ -39,5 +51,22 @@ public class Main {
             System.out.println("IOException: " + e.getMessage());
           }
         }
+  }
+
+    private static void processRequest(Socket clientSocket)  {
+
+      try {
+          String clientInput = null;
+          OutputStream outputStream = clientSocket.getOutputStream();
+          BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+          while ((clientInput = bufferedReader.readLine()) != null) {
+              if (clientInput.equalsIgnoreCase("ping")) {
+                  outputStream.write("+PONG\r\n".getBytes());
+              }
+          }
+      }
+      catch (Exception e){
+          System.out.println(e);
+      }
   }
 }

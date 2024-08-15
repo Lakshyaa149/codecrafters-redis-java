f492000e056394b9175200ae1c531dc17a530c8e
lakshay <lakshay@lakshays-MacBook-Pro.local>
2024-08-15 13:54:24 +0530

    pass stage

--- src/main/java/RedisProtocolHandler.java
+++ src/main/java/RedisProtocolHandler.java
@@ -1,29 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class RedisProtocolHandler {
 
-    public void handleRequestAndResponse(Socket socket) throws IOException {
+    public void  handleRequestAndResponse(Socket socket) throws IOException {
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        int  line = -1;
-        while(( line = bufferedReader.read())!=-1) {
+        OutputStream outputStream = new BufferedOutputStream(socket.getOutputStream());
 
+        int ascii = bufferedReader.read();
+        if((char)ascii == '*'){
+            ascii = bufferedReader.read();
+            for(int i=0;i<ascii;i++){
+                ascii = bufferedReader.read();
+                if((char)ascii == '$'){
+                    outputStream.write(handleBulkString(bufferedReader).getBytes());
+                }
+            }
 
-            System.out.println("line " + line);
         }
-        String response = null;
-//
-//        if(line.equalsIgnoreCase("echo")) {
-//            bufferedReader.readLine();
-//            line = bufferedReader.readLine();
-//            response = "$" + line.length() + "\r\n" + line + "\r\n";
-//        }
-//        System.out.println("response" +response);
-//        socket.getOutputStream().write(response.getBytes());
     }
 
+    private String handleSimpleString(BufferedReader bufferedReader) throws IOException {
+        int ascii = -1;
+        StringBuilder sb = new StringBuilder();
+        while ((ascii =bufferedReader.read())!=13){
+            sb.append((char)ascii);
+        }
+        return sb.toString();
+    }
+
+    private static String handleBulkString(BufferedReader bufferedReader) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String line;
+        int ascii;
+        // $3\r\nhey\r\n
+        int noOfCharacters = bufferedReader.read();
+        skipCharacters(bufferedReader);
+        for(int i=0; i<noOfCharacters;i++){
+            sb.append((char)bufferedReader.read());
+        }
+        skipCharacters(bufferedReader);
+        return sb.toString();
+    }
+
+    private static void skipCharacters(BufferedReader bufferedReader) throws IOException {
+        bufferedReader.skip(2);
+    }
 }

970d1642a0d59ecf277133efc576d304ee3a895e
lakshay <lakshay@lakshays-MacBook-Pro.local>
2024-08-15 13:34:29 +0530

    pass stage

--- src/main/java/RedisProtocolHandler.java
+++ src/main/java/RedisProtocolHandler.java
@@ -9,18 +9,21 @@ public class RedisProtocolHandler {
     public void handleRequestAndResponse(Socket socket) throws IOException {
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        bufferedReader.readLine();
-        String line = bufferedReader.readLine();
-        System.out.println("line "+ line);
-        String response = null;
 
-        if(line.equalsIgnoreCase("echo")) {
-            bufferedReader.readLine();
-            line = bufferedReader.readLine();
-            response = "$" + line.length() + "\r\n" + line + "\r\n";
+        while((line = bufferedReader.readLine())!=null) {
+
+            String line = bufferedReader.readLine();
+            System.out.println("line " + line);
         }
-        System.out.println("response" +response);
-        socket.getOutputStream().write(response.getBytes());
+        String response = null;
+//
+//        if(line.equalsIgnoreCase("echo")) {
+//            bufferedReader.readLine();
+//            line = bufferedReader.readLine();
+//            response = "$" + line.length() + "\r\n" + line + "\r\n";
+//        }
+//        System.out.println("response" +response);
+//        socket.getOutputStream().write(response.getBytes());
     }
 
 }
